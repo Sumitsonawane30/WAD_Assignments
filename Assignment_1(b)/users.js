3f71be0aeb628b73206f8383dd2d6cecb2ece0e1
Sumit I Sonawane <132132305+Sumitsonawane30@users.noreply.github.com>
2025-02-13 14:37:35 +0530

    Added delete functionality

--- Assignment_1(b)/users.js
+++ Assignment_1(b)/users.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     let users = JSON.parse(localStorage.getItem("users")) || [];
     let userList = $("#userList");
 
+
     if (users.length === 0){
         userList.html("<li> No users registered yet.</li>");
     }
@@ -11,4 +12,25 @@ $(document).ready(function(){
             userList.append(`<li> Name: ${user.name}, Email: ${user.email}</li>`)
         });
     }
-});
+    },
+    $("#del").click( function(){
+
+        //Store in local storage
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        
+        users.pop(users);
+        localStorage.setItem("users", JSON.stringify(users));
+
+        let userList = $("#userList");
+
+
+        if (users.length === 0){
+            userList.html("<li> No users registered yet.</li>");
+        }
+        else{
+        userList.html("")
+        users.forEach(user =>{
+             userList.append(`<li> Name: ${user.name}, Email: ${user.email}</li>`)
+            });
+        }
+    }))
